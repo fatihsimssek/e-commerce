ad2e901b65986f5ad28ef82dd2770ec130942eaf
mehmet fatih şimşek <147408701+fatihsimssek@users.noreply.github.com>
2025-01-29 21:01:50 +0300

    [FO-1] - navbar sidebar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,45 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+      },
+    ],
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    config.stats = {
+      warnings: false,
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
---
+++ src/app/components/Sidebar/index.jsx
@@ -0,0 +1,65 @@
+"use client";
+import Logo from "../../../../public/icons/Logo.svg";
+import HomeIcon from "../../../../public/icons/HomeIcon";
+import DiscountIcon from "../../../../public/icons/DiscountIcon";
+import DashboardIcon from "../../../../public/icons/DashboardIcon";
+import MessageIcon from "../../../../public/icons/MessageIcon";
+import NotificationIcon from "../../../../public/icons/NotificationIcon";
+import SettingIcon from "../../../../public/icons/SettingIcon";
+import LogoutIcon from "../../../../public/icons/Logout";
+import styles from "./Sidebar.module.scss";
+import { useState } from "react";
+import Link from "next/link";
+
+const menuItems = [
+  { id: "home", Icon: HomeIcon, label: "Home", href: "/" },
+  { id: "discount", Icon: DiscountIcon, label: "Discount", href: "/discount" },
+  {
+    id: "dashboard",
+    Icon: DashboardIcon,
+    label: "Dashboard",
+    href: "/dashboard",
+  },
+  { id: "messages", Icon: MessageIcon, label: "Messages", href: "/messages" },
+  {
+    id: "notifications",
+    Icon: NotificationIcon,
+    label: "Notifications",
+    href: "/notifications",
+  },
+  { id: "settings", Icon: SettingIcon, label: "Settings", href: "/settings" },
+  { id: "logout", Icon: LogoutIcon, label: "Logout", href: "/logout" },
+];
+
+const Sidebar = () => {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const handleItemClick = (id) => {
+    setActiveItem(id);
+  };
+
+  return (
+    <nav className={styles.sidebar}>
+      <Logo className={styles.logo} />
+      <div className={styles.menuItems}>
+        {menuItems.map(({ id, Icon, label, href }) => {
+          return (
+            <Link key={id} href={href} onClick={() => handleItemClick(id)}>
+              <div
+                className={activeItem === id ? styles.active : ""}
+                aria-label={label}
+              >
+                <Icon
+                  rectFill={activeItem === id ? "#EA7C69" : "transparent"}
+                  fill={activeItem === id ? "white" : "#EA7C69"}
+                />
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
+
+export default Sidebar;
