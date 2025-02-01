db815eca40d4b36205e578a50571d25c142a27be
mehmet fatih şimşek <147408701+fatihsimssek@users.noreply.github.com>
2025-02-02 00:01:29 +0300

    [FO-1] - Navbar header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,27 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import "./global.css";
 
 const barlow = localFont({
   src: [
     {
-      path: "./fonts/Barlow-Regular.ttf",
+      path: "./fonts/Barlow-Black.ttf",
       weight: "400",
       style: "normal",
+    },
+    {
+      path: "./fonts/Barlow-BlackItalic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Barlow-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Barlow-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
     },
     {
       path: "./fonts/Barlow-Medium.ttf",
@@ -14,16 +29,43 @@ const barlow = localFont({
       style: "normal",
     },
     {
+      path: "./fonts/Barlow-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Barlow-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Barlow-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
       path: "./fonts/Barlow-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts/Barlow-Bold.ttf",
-      weight: "700",
+      path: "./fonts/Barlow-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Barlow-Thin.ttf",
+      weight: "100",
       style: "normal",
     },
+    {
+      path: "./fonts/Barlow-ThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
   ],
+  variable: "--font-barlow",
+  display: "swap",
 });
 
 export const metadata = {
